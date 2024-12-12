f3e74e59a7e54a8f8cb94e9f4b7b5c4822891d39
andreasbbusk <144665042+andreasbbusk@users.noreply.github.com>
2024-12-12 08:39:02 +0100

    Setting up pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/https://github.com/andreasbbusk/vmh.git/"}>
       <App />
     </BrowserRouter>
   </StrictMode>,
