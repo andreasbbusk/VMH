47c4ef811472c4782097323f119bb5a6e2d97f4f
OliverBoesen <145548550+OliverBoesen@users.noreply.github.com>
2024-12-13 01:38:43 +0100

    Add files via upload

--- src/pages/Hudcancer.jsx
+++ src/pages/Hudcancer.jsx
@@ -1,40 +1,229 @@
-import { motion as m } from "framer-motion";
-import { Link } from "react-router-dom";
 import styles from "./Hudcancer.module.css";
+import { useState, useRef } from "react";
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
+import HudcancerIntro from "../assets/hudcancer-intro.jpg";
+import Lotte from "../assets/LotteDamkjær.mp4";
+import Play from "../assets/play-button.svg";
+import thumbnail from "../assets/LotteThumbnail.png";
+
+import parasol from "../assets/parasol.svg";
+import linjeop from "../assets/linjeop.svg";
+import solhat from "../assets/solhat.svg";
+import solhatpil from "../assets/solhat-pil.svg";
+import solcreme from "../assets/solcreme.svg";
+import solcremepil from "../assets/solcreme-pil.svg";
 
 const Hudcancer = () => {
+  const [isOverlayVisible, setIsOverlayVisible] = useState(true); // Styrer overlay-knappen
+  const [controlsVisible, setControlsVisible] = useState(false); // Skjuler kontroller initialt
+  const videoRef = useRef(null); // Reference til videoelementet
+
+  const handleOverlayClick = () => {
+    if (videoRef.current) {
+      videoRef.current.play(); // Starter videoen
+      setControlsVisible(true); // Gør kontrollerne synlige
+    }
+    setIsOverlayVisible(false); // Fjerner overlay
+  };
+
   return (
     <>
-      <m.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.75, ease: "easeOut" }}
-      >
-        <div className={styles.container}>
-        <section className={styles.project_video}>
-            <div className={styles.project_video_text}>
-              <h2>
-                Projekterne bidrager til <strong>forskning og udvikling</strong>
-              </h2>
-              <div className={styles.horizontal_line_video}></div>
-              <p>
-                Hør Justins historie om hvordan rigtig og hurtig hjælp gav ham
-                livsglæden tilbage efter kræft. Vi ønsker at hjælpe borgere i
-                vores område til en bedre forebyggelse mod hudcancer og til en
-                hurtigere og nemmere behandling, hvis de har fået hudcancer.{" "}
-                <strong>Med din støtte kan vi hjælpe endnu flere</strong>
-              </p>
-              <Link className={['support-button']} to="/Stoet-nu">
-                Støt nu
-              </Link>
-            </div>
-            <div>
-              <video src="" autoPlay muted loop></video>
-            </div>
-          </section>
+      <Breadcrumb />
+      <div className={styles["hudcancer-intro-container"]}>
+        <div className={styles["hudcancer-intro-tekst"]}>
+          <h1>
+            <strong>tidlig diagnose er vigtigt</strong> <br />
+            undersøg din hud <br /> jævnligt
+          </h1>
+          <hr className={styles["hudcancer-divider"]} />
+          <p>
+            Flere og flere mennesker i Danmark får modermærkekræft, og det samme
+            gælder almindelig hudcancer. Modermærkekræft er en alvorlig sygdom,
+            som kan være dødelig, det er derfor meget vigtigt, at det opdages og
+            behandles så tidligt som muligt.
+          </p>
         </div>
-      </m.main>
+
+        <div className={styles["hudcancer-intro-billede"]}>
+          <img src={HudcancerIntro} alt="Hudcancer introduktion" />
+        </div>
+      </div>
+
+      <div className={styles["pletter-sektion"]}>
+        <div className={styles["hudcancer-pletter-container"]}>
+          <div className={styles["hudcancer-pletter-se"]}>
+            <h1>
+              <strong>Se efter</strong>
+              <br /> pletter som:
+            </h1>
+            <p>
+              Søg læge hvis du er i tvivl <br />
+              <a href="#">Klik her og lær, hvor du skal tjekke</a>
+            </p>
+          </div>
+
+          <div className={styles["hudcancer-pletter-symptomer"]}>
+            <h3>ændrer størrelse, farve og/eller form</h3>
+            <hr className={styles["pletter-divider"]} />
+            <h3>ser anderledes ud end dine andre pletter, er asymmetriske</h3>
+            <hr className={styles["pletter-divider"]} />
+            <h3>
+              føles ru eller skæller - nogle gange kan
+              <br />
+              du mærke forandringerne før du kan se dem
+            </h3>
+            <hr className={styles["pletter-divider"]} />
+            <h3>har mange farver, klør, bløder eller væsker</h3>
+            <hr className={styles["pletter-divider"]} />
+            <h3>
+              har en skinnende overflade eller <br /> ligner et sår, som ikke
+              vil hele.
+            </h3>
+            <hr className={styles["pletter-divider"]} />
+            <div className={styles["pletter-kilde"]}>
+              <p>
+                Kilde: <a href="#">Euromelanoma</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["saar-container"]}>
+        <div className={styles["saar-video"]}>
+          <div className={styles["video-wrapper"]}>
+            <video
+              ref={videoRef}
+              src={Lotte}
+              controls={controlsVisible}
+              poster={thumbnail}
+            ></video>{" "}
+            {/* Tilføjer controls kun når videoen spiller */}
+            {isOverlayVisible && (
+              <div
+                className={styles["video-overlay"]}
+                onClick={handleOverlayClick}
+                role="button"
+                tabIndex={0}
+                aria-label="Afspil video"
+              >
+                <img src={Play} alt="Play Button" />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className={styles["saar-tekst"]}>
+          <h1>
+            <strong>Et sår der</strong> <br /> ikke ville hele
+          </h1>
+          <hr className={styles["saar-divider"]} />
+          <p>
+            Hør Lottes historie om hvordan et sår der ikke ville hele <br />
+            kom tilbage igen og igen. I dag er hun ekstra opmærksom, <br />
+            på at tjekke hele sin krop.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles["forebygge-background"]}>
+      <div className={styles["forebygge-overskrift-mobil"]}>
+          <h1>
+            sådan <bold> Forebygger du </bold> <br />
+            udviklingen af hudkræft
+          </h1>
+          <p>
+            Kilde: <a href="#">Kræftens bekæmpelse</a>
+          </p>
+        </div>
+        <div className={styles["forebygge-skygge"]}>
+          <img src={parasol} alt="" />
+          <div className={styles["forebygge-skygge-tekst"]}>
+            <h2>Skygge</h2>
+            <p>
+              Solens uv-stråling er stærkest mellem 12 og 15. Næsten <br />
+              halvdelen af uv-strålingen falder i dette tidsrum. Derfor kan du
+              <br />
+              undgå rigtig meget uv-stråling ved at finde skygge midt på <br />
+              dagen.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles["linjeop"]}>
+          <img src={linjeop} alt="" />
+        </div>
+
+        <div className={styles["forebygge-overskrift"]}>
+          <h1>
+            sådan <bold> Forebygger du </bold> <br />
+            udviklingen af hudkræft
+          </h1>
+          <p>
+            Kilde: <a href="#">Kræftens bekæmpelse</a>
+          </p>
+        </div>
+
+<div className={styles['infographic-container']}>
+            <div className={styles['solhat-container']}>
+            <div className={styles['Solhat-billede-container']}>
+            <img src={solhat} alt="" />
+            <div className={styles['mobil-farvel-1']}>
+            <img src={solhatpil} alt="" />
+            </div>
+            </div>
+            <div className={styles['solhat-tekst']}>
+            <h2>Solhat/tøj</h2>
+            <p>
+            Hvis du ikke har mulighed for at være i skyggen er en solhat med <br />
+             bred skygge og en langærmet t-shirt den bedste beskyttelse mod <br /> solens uv-stråling.</p>
+             </div>
+            </div>
+
+
+            <div className={styles['solcreme-container']}>
+            <div className={styles['Solcreme-billede-container']}>
+            <div className={styles['mobil-farvel']}>
+            <img src={solcremepil} alt="" />
+            </div>
+            <img src={solcreme} alt="" />
+            </div>
+            <div className={styles['sol-creme-tekst']}>
+            <h2>Solcreme</h2>
+            <p>
+          Husk altid rigeligt med solcreme. Efter badning og aftørring bør <br />
+           du smøre igen. Brug faktor 15 eller mere i Danmark og faktor 30 <br />
+            eller mere, når du er ude og rejse i lande, der har et varmere <br />
+             klima. Solcremen skal være bredspektret, dvs. beskytte mod både <br />
+              UVA- og UVB-stråling.</p>
+              </div>
+            </div>
+            </div>
+      </div>
+
+      <div className={styles['yderlige-info-background']}>
+      <div className={styles['yderlige-info-overkskrift']}>
+      <h1>For Yderligere Information</h1>
+      </div>
+      <div className={styles['yderlige-info-container']}>
+        <div className={styles['yderlige-info-links']}>
+          <p>Kræftens Bekæmpelse:</p>
+          <a href="">Hudkræft: Symptomer, behandling, undersøgelser, diagnose og årsager</a> <br />
+          <a href="">Statistik om modermærkekræft</a> <br />
+          <a href="">Statistik om basalcelle- og pladecellekræft samlet</a>
+        </div>
+
+        <div className={styles['yderlige-info-links']}>
+          <p>Euromelanoma:</p>
+          <a href="">Fakta om hudkræft</a> <br />
+          <a href="">Forebyggelse af hudkræft</a> <br />
+          <a href="">Hvad skal du kigge efter</a>
+        </div>
+
+        <div className={styles['yderlige-info-links']}>
+          <a href="">Solkampagnen</a> <br />
+        </div>
+      </div>
+      </div>
     </>
   );
 };
---
+++ src/pages/OmOs.jsx
@@ -0,0 +1,144 @@
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
+import styles from "./OmOs.module.css";
+import OmOsVideo from "../assets/OmOsVideo.mp4"
+import { useState, useRef } from "react";
+import Play from "../assets/play-button.svg";
+import Ideenbag1 from "../assets/IdeenBag1.svg"
+import Ideenbag2 from "../assets/IdeenBag2.svg"
+import Ideenbag3 from "../assets/IdeenBag3.svg"
+import VMVStotte from "../assets/VMVStotte.svg"
+import VMVSammen from "../assets/VMVSammen.svg"
+import VMVLokale from "../assets/VMVLokale.svg"
+
+
+
+const OmOs = () => {
+const [isOverlayVisible, setIsOverlayVisible] = useState(true); // Styrer overlay-knappen
+  const [controlsVisible, setControlsVisible] = useState(false); // Skjuler kontroller initialt
+  const videoRef = useRef(null); // Reference til videoelementet
+
+  const handleOverlayClick = () => {
+    if (videoRef.current) {
+      videoRef.current.play(); // Starter videoen
+      setControlsVisible(true); // Gør kontrollerne synlige
+    }
+    setIsOverlayVisible(false); // Fjerner overlay
+  };
+return (
+<>
+
+<Breadcrumb />
+
+<div className={styles['omos-intro-container']}>
+<div className={styles['omos-intro-venstre']}>
+<h1>Vi skaber <br /> <bold>Forandring</bold> Sammen</h1>
+<hr className={styles["omos-divider"]} />
+<p>Is simply dummy text of the printing and typesetting industry. <br />
+ Lorem Ipsum has been the industrys standard dummy text ever <br />
+  since the 1500s, when an unknown printer took a galley of type and <br />
+  scrambled it to make a type specimen book. It has survived not <br />
+ only five centuries, but also the leap into electronic typesetting. </p>
+</div>
+
+<div className={styles['omos-intro-hojre']}>
+<div className={styles["video-wrapper"]}>
+            <video
+              ref={videoRef}
+              src={OmOsVideo}
+              controls={controlsVisible}
+            ></video>{" "}
+            {/* Tilføjer controls kun når videoen spiller */}
+            {isOverlayVisible && (
+              <div
+                className={styles["video-overlay"]}
+                onClick={handleOverlayClick}
+                role="button"
+                tabIndex={0}
+                aria-label="Afspil video"
+              >
+                <img src={Play} alt="Play Button" />
+              </div>
+            )}
+          </div>
+</div>
+</div>
+
+<div className={styles['omos-citat']}>
+<h1>
+"Vi har startet vejle mod hudcancer, <br />
+ fordi hudkræft er et større problem <br />
+ og det skal der endnu mere fokus på" <br />
+ </h1>
+ <p>-Dorthe, Susanne, Christina, Birgitte </p>
+</div>
+
+<div className={styles['ideenbag-container']}>
+<div className={styles['ideenbag-billede-container']}>
+<div className={styles['ideenbag-billede-et-container']}>
+<img src={Ideenbag1} alt="" />
+<img src={Ideenbag2} alt="" />
+</div>
+<div className={styles['ideenbag-billede-to-container']}>
+<img src={Ideenbag3} alt="" />
+</div>
+</div>
+<div className={styles['ideenbag-tekst']}>
+<h1> <bold>Ideen bag</bold> <br /> 
+Vejle mod hudcancer</h1>
+<hr className={styles["ideenbag-divider"]} />
+<p>is simply dummy text of the printing and typesetting <br />
+ industry. Lorem Ipsum has been the industrys standard <br />
+dummy text ever since the 1500s, when an unknown printer <br />
+took a galley of type and scrambled it to make a type <br />
+specimen book. It has survived not only five centuries, but <br />
+also the leap into electronic typesetting, </p>
+</div>
+</div>
+
+<div className={styles['VMV-container']}>
+    <h1>vision, mission & værdier</h1>
+<div className={styles['VMV-content']}>
+<div className={styles['VMV-stotte']}>
+<img src={VMVStotte} alt="" />
+<div className={styles['VMV-stotte-tekst']}>
+<p> <bold>med din støtte</bold> <br />
+kan vi samle ind til <br />
+kampen mod hudcancer</p>
+</div>
+</div>
+<div className={styles['VMV-sammen']}>
+<img src={VMVSammen} alt="" />
+<div className={styles['VMV-stotte-tekst']}>
+<p> <bold>Sammen kan</bold> <br />
+vi opnå store <br />
+Resultater</p>
+</div>
+</div>
+<div className={styles['VMV-lokale']}>
+<img src={VMVLokale} alt="" />
+<div className={styles['VMV-stotte-tekst']}>
+<p> <bold>gennem lokale</bold> <br />
+midler kan vi gavne <br />
+kræftcenteret i vejle</p>
+</div>
+</div>
+</div>
+
+
+</div>
+
+<div className={styles['omos-kontakt-container']}>
+<div className={styles['omos-kontakt-tekst']}>
+    <h1>Har du et spørgsmål?</h1>
+    <p>Du er velkommen til at kontakte os og finde ud af hvordan du <br />
+    kan være med i kampen mod hudcancer</p>
+    <button className={styles['intro-button']}>Kontakt os</button>
+</div>
+<div className={styles['omos-kontakt-knap']}></div>
+</div>
+</>
+); 
+};
+
+export default OmOs;
+
